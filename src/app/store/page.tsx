f6889b7a97a70142945be8e6dcda3f59aceb62e1
jianzhe-w-sectalabs <jianzhe.woo@secta5.com>
2024-05-03 12:29:41 +0800

    Add getproducts library method

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,17 +1,5 @@
+import getProducts from "@/lib/getProducts"
 import React from "react"
-
-const getProducts = async () => {
-  // no cache for get products
-  const res = await fetch("http://localhost:8000/api/v1/products", {
-    cache: "no-store",
-  })
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data")
-  }
-
-  return res.json()
-}
 
 const Store: React.FC = async () => {
   const productsData = getProducts()
