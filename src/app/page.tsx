80054f649fbb5c5074c8ee9dc09168e9200aa2ad
jianzhe-w-sectalabs <jianzhe.woo@secta5.com>
2024-05-03 11:22:32 +0800

    Creata basic lading page layout and loading ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
 import React from "react"
 
+const Landing: React.FC = () => {
+  return (
+    <div className="h-[calc(100vh-60px)]">
+      <div className="flex flex-row">
+        <div style={{ fontSize: "70px" }}>CYBER</div>
+
+        <div className="font-light">[コマース]</div>
+      </div>
+
+      <div>Ticker</div>
+
+      <div>Bottom Container</div>
+    </div>
+  )
+}
+
+const Credits: React.FC = () => {
+  return <div className="bg-red-500">Credits</div>
+}
+
 const Home: React.FC = () => {
-  return <div>this is the main page</div>
+  return (
+    <div className="w-screen">
+      <Landing />
+
+      <Credits />
+    </div>
+  )
 }
 
 export default Home
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,8 +5,12 @@ const Navbar: React.FC = () => {
   return (
     <div className="flex justify-between h-[60px] px-[40px] py-[20px]">
       <Link href="/">HOME</Link>
-      <Link href="/store">STORE</Link>
-      <Link href="/login">LOGIN</Link>
+      <Link href="/store" prefetch={true}>
+        STORE
+      </Link>
+      <Link href="/login" prefetch={true}>
+        LOGIN
+      </Link>
     </div>
   )
 }
