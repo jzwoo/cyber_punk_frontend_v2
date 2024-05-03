0502d7f612aa4bf6f9d3ae380c4e15209f6dccee
jianzhe-w-sectalabs <jianzhe.woo@secta5.com>
2024-05-03 12:00:42 +0800

    Create basic login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,86 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
 import React from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const loginFormSchema = z.object({
+  username: z.string().min(2),
+  password: z.string().min(2),
+})
 
 const Login: React.FC = () => {
-  return <div>this is the login page</div>
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof loginFormSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <div className="flex justify-center items-center h-full">
+      <Form {...form}>
+        <form
+          className="flex flex-col gap-4 w-[400px]"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+
+                <FormControl>
+                  <Input placeholder="username" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+
+                <FormControl>
+                  <Input placeholder="password" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button variant="secondary" className="mt-5 w-full" type="submit">
+            LOGIN
+          </Button>
+        </form>
+      </Form>
+    </div>
+  )
 }
 
 export default Login
