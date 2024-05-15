319ac5b805d10bc48419f38c1869701660fb850c
jianzhe-w-sectalabs <jianzhe.woo@secta5.com>
2024-05-15 23:05:45 +0800

    Added landing page images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { robotoMonoFont } from "@/app/font"
 import Ticker from "@/components/ticker/ticker"
+import Image from "next/image"
 import React from "react"
 
 const Landing: React.FC = () => {
@@ -19,7 +20,7 @@ const Landing: React.FC = () => {
 
       <Ticker />
 
-      <div className="flex flex-1 p-10 justify-between gap-8">
+      <div className="flex flex-1 p-10 justify-between gap-10">
         <div className="w-[500px]">
           <p
             className={`${robotoMonoFont.className} text-xl text-justify leading-[1.8] tracking-[8px]`}
@@ -37,16 +38,50 @@ const Landing: React.FC = () => {
           </p>
         </div>
 
-        <div className="flex flex-1 bg-blue-500">image 1</div>
+        <div className="relative flex flex-[2_2_0%]">
+          <Image
+            className="object-cover"
+            src="/landing/image1.jpg"
+            alt="Image1"
+            fill={true}
+            quality={100}
+          />
+        </div>
 
-        <div className="flex flex-1 bg-green-500">image 2</div>
+        <div className="relative flex flex-col flex-1">
+          <div className="relative flex flex-1 mt-[-286px] mb-[130px]">
+            <Image
+              className="object-cover"
+              src="/landing/image2.jpg"
+              alt="Image2"
+              fill={true}
+              quality={100}
+            />
+          </div>
+
+          <div className="absolute flex flex-col items-center right-[-29.5px] bottom-[29.5px] -rotate-90 leading-none">
+            <a href="#credits" className="tracking-wide">
+              SCROLL
+            </a>
+
+            <a href="#credits">
+              <span className="text-[var(--gold)] font-light">
+                [スクロール]
+              </span>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
 const Credits: React.FC = () => {
-  return <div className="bg-red-500">Credits</div>
+  return (
+    <div id="credits" className="bg-blue-500 h-[calc(100vh-60px)]">
+      Credits
+    </div>
+  )
 }
 
 const Home: React.FC = () => {
